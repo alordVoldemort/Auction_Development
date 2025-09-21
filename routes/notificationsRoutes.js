19a62609a57c4ff222a5d7e55f9b8148af883018
Pooja Masne <143479102+Poojamasne@users.noreply.github.com>
2025-09-21 21:12:47 +0530

    Create notificationsRoutes.js

---
+++ routes/notificationsRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+// const { authMiddleware } = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.use(authMiddleware.authenticateToken);
+
+const {
+  getMyNotifications,
+  getParticipantNotifications
+} = require('../controllers/notificationsController');
+
+router.get('/my-notification',  getMyNotifications);
+router.get('/notifications/participants/:auctionId',  getParticipantNotifications);
+
+module.exports = router;
